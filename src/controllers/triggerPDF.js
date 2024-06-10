3d7c1f61d5ccd9891bd8104d80b515c8f656485f
David Valdez Gramajo <davidpivito@gmail.com>
2024-06-09 23:37:44 -0300

    adding line

--- src/controllers/triggerPDF.js
+++ src/controllers/triggerPDF.js
@@ -5,7 +5,7 @@ const triggerPF = (req, res) => {
     const numRandom = Math.floor(Math.random()*100)
 
     //crea el PDF
-    const doc = new PDFDocument({size: 'A4'});
+    const doc = new PDFDocument({size: 'A4', margin: 50});
 
     //empieza el contenido del PDF
     doc.fontSize(30);
@@ -14,18 +14,35 @@ const triggerPF = (req, res) => {
     doc.text('Nombre Empresa.')
     doc.text('Direccion Empresa')
     doc.moveDown()
-    doc.text("CLIENTE", 50, 150)
+    doc.text("CLIENTE", 50, 100)
     doc.text("Nombre Cliente")
     doc.text("Direccion Cliente")
 
-    doc.text("N° COMPROBANTE", 300, 150)
-    doc.text("23423", 450, 150)
-    doc.text("FECHA", 300, 160)
-    doc.text("20/12/2024", 450, 160)
-    doc.text("N° DE PEDIDO", 300, 170)
-    doc.text("23423", 450, 170)
-    doc.text("FECHA DE VENCIMIENTO", 300, 180)
-    doc.text("20/12/2024", 450, 180)
+    doc.text("N° COMPROBANTE", 350, 100)
+    doc.text("23423", 450, 100, {align: 'right'})
+    doc.text("FECHA", 350, 110)
+    doc.text("20/12/2024", 450, 110, {align: 'right'})
+    doc.text("N° DE PEDIDO", 350, 120)
+    doc.text("23423", 450, 120, {align: 'right'})
+    doc.text("FECHA DE VENCIMIENTO", 350, 130)
+    doc.text("20/12/2024", 450, 130, {align: 'right'})
+    doc.moveDown()
+    
+    doc.moveTo(50, 145)  // Punto de inicio de la línea
+   .lineTo(545, 145)  // Punto final de la línea
+   .stroke()
+
+    doc.text("CANT.", 60, 150)
+    doc.text("DESCRIPCIÓN", 180, 150)
+    doc.text("PRECIO U.", 350, 150)
+    doc.text("IMPORTE", 490, 150)
+    
+    doc.moveTo(50, 165)  // Punto de inicio de la línea
+    .lineTo(545, 165)  // Punto final de la línea
+    .stroke()
+    
+    doc.text("1222", 70, 170, {align: 'left'})
+
     //finaliza el contenido del PDF
 
     //finaliza el PDF
